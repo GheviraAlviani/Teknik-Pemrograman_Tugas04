8cc8d6c12d7650849a7409f357fc81332a157197
GheviraAlviani <ghevira.alviani.tif425@polban.ac.id>
2026-03-06 05:20:03 +0700

    Update Sorting.java

--- PolymorphicSorting/src/Sorting.java
+++ PolymorphicSorting/src/Sorting.java
@@ -41,17 +41,18 @@ if (list[scan].compareTo(list[min]) < 0)
 //-----------------------------------------------------------------
 public static void insertionSort (Comparable[] list)
 {
-for (int index = 1; index < list.length; index++)
-{
-Comparable key = list[index];
-int position = index;
-// Shift larger values to the right
-while (position > 0 && key.compareTo(list[position-1]) < 0)
-{
-list[position] = list[position-1];
-position--;
-}
-list[position] = key;
+    for (int index = 1; index < list.length; index++)
+    {
+        Comparable key = list[index];
+        int position = index;
+
+        while (position > 0 && key.compareTo(list[position-1]) > 0)
+        {
+            list[position] = list[position-1];
+            position--;
         }
+
+        list[position] = key;
     }
+}
 }
